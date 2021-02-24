56fc7a5c268b21ac775d4c6d3e367161c8e4a699
Selwyn <talk@selwyn.cc>
2021-02-24 16:35:15 +0100

    Improve feedback on badge services search field input

--- tiny-shields-website/src/components/badge-builder.jsx
+++ tiny-shields-website/src/components/badge-builder.jsx
@@ -1,30 +1,67 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 import { withHydrate } from 'microsite/hydrate';
+import CopyIcon from './copy-icon';
 import services from 'services';
 
 export default withHydrate(function BadgeBuilder() {
   const [searchInput, setSearchInput] = useState('');
+  const [filteredServices, setFilteredServices] = useState([]);
+  const [copiedInput, setCopiedInput] = useState(false);
+
+  useEffect(() => {
+    setFilteredServices(Object.entries(services).filter(
+      ([key]) => key.startsWith(searchInput.split('/')[0])
+    ));
+  }, [searchInput]);
+
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    navigator.clipboard
+      .writeText(`![${filteredServices[0][0]}](${getUrlFromPath(searchInput)})`)
+      .then(() => {
+        setCopiedInput(true);
+        setTimeout(() => setCopiedInput(false), 3000);
+      })
+      .catch(console.error);
+  }
 
   return (
     <>
       <img
         class="mb-6 h-5"
-        src={`https:/tinyshields.dev/${searchInput}`}
+        src={
+          filteredServices.length && searchInput.length >= 3
+          ? getUrlFromPath(searchInput)
+          : '/assets/dummy-badge.svg'
+        }
         id="badgePreview"
         aria-live="polite"
       />
-      <label class="flex w-full p-2 mb-6 dark:text-black dark:bg-white border-pink-400 border-l-2 pl-2 shadow rounded">
-        https://tinyshields.dev/
-        <input
-          class="w-full bg-transparent"
-          value={searchInput}
-          onInput={(event) => setSearchInput(event.target.value)}
-          aria-controls="servicesList badgePreview"
-          autoFocus
-        />
-      </label>
+      <form
+        class="w-full mb-6 dark:text-black dark:bg-white"
+        onSubmit={handleSearchSubmit}
+      >
+        <label class="relative flex w-full border-pink-400 border-l-2 pl-2 shadow rounded focus-within:ring-1 focus-within:ring-pink-400">
+          <span class="sr-only">Search by service</span>
+          <input
+            class="w-full bg-transparent focus:outline-none "
+            value={searchInput}
+            onInput={(event) => setSearchInput(event.target.value)}
+            aria-controls="servicesList badgePreview"
+            autoFocus
+          />
+          <button class="flex items-center p-2 bg-gray-200 font-semibold hover:bg-gray-300 transition-colors">
+            <CopyIcon />
+            <span class="ml-1">Copy</span>
+          </button>
+          <span
+            class={`absolute right-2 bottom-full mb-2 transition-opacity ${!copiedInput && 'opacity-0'}`}>
+            Copied!
+          </span>
+        </label>
+      </form>
       <ul id="servicesList" aria-live="polite">
-        {Object.entries(services).filter(([key]) => key.startsWith(searchInput.split('/')[0])).map(([key, { routes }]) => (
+        {filteredServices.map(([key, { routes }]) => (
           <li class="flex mb-4">
             <span class="w-32 font-bold text-right">{key}</span>
             <ul class="w-96">
@@ -38,3 +75,7 @@ export default withHydrate(function BadgeBuilder() {
     </>
   );
 });
+
+function getUrlFromPath(path) {
+  return `https:/tinyshields.dev/${path}.svg`
+}
