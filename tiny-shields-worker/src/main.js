be56fd3ad8c173a42672c4d2f67724909e6502ec
Selwyn <talk@selwyn.cc>
2021-02-24 11:18:58 +0100

    Flatten query value for deep value retrieval

--- tiny-shields-worker/src/main.js
+++ tiny-shields-worker/src/main.js
@@ -1,5 +1,6 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 import { exec, match, parse } from 'matchit';
+import { flatten } from 'flattenizer';
 import tinyBadgeMaker from 'tiny-badge-maker';
 import services from 'services';
 
@@ -10,20 +11,13 @@ addEventListener('fetch', (event) => {
 async function handleRequest(event) {
   const { pathname, searchParams } = new URL(event.request.url);
 
-  if (
-    pathname.startsWith('/assets') ||
-    !pathname.endsWith('.svg')
-  ) {
-    return getAssetFromKV(event);
-  }
-
   if (pathname === '/live') {
     return fetch(searchParams.get('url'))
       .then((response) => response.json())
       .then((data) => (
         new Response(
           tinyBadgeMaker({
-            message: data[searchParams.get('query')],
+            message: flatten(data)[searchParams.get('query')],
             label: searchParams.get('label'),
             color: searchParams.get('color'),
           }),
@@ -36,33 +30,40 @@ async function handleRequest(event) {
           },
         )
       ));
-  } else {
-    const { name } = exec(
-      pathname,
-      match(pathname, [parse('/:name/*')])
-    );
-
-    if (!name || !services[name]) {
-      return new Response('Invalid service', { status: 400 });
-    }
-
-    const routeValues = exec(
-      pathname,
-      match(pathname, services[name].routes.map((route) =>
-        parse(`/:name${route}.svg`)
-      ))
-    );
-
-    return services[name].handler(routeValues)
-      .then((badgeData) => new Response(
-        tinyBadgeMaker(badgeData),
-        {
-          headers: {
-            'Content-Type': 'image/svg+xml',
-            'Cache-Control': `max-age=${60 * 5}`,
-            'Content-Disposition': 'inline',
-          },
-        }
-      ));
   }
+
+  if (
+    pathname.startsWith('/assets') ||
+    !pathname.endsWith('.svg')
+  ) {
+    return getAssetFromKV(event);
+  }
+
+  const { name } = exec(
+    pathname,
+    match(pathname, [parse('/:name/*')])
+  );
+
+  if (!name || !services[name]) {
+    return new Response('Invalid service', { status: 400 });
+  }
+
+  const routeValues = exec(
+    pathname,
+    match(pathname, services[name].routes.map((route) =>
+      parse(`/:name${route}.svg`)
+    ))
+  );
+
+  return services[name].handler(routeValues)
+    .then((badgeData) => new Response(
+      tinyBadgeMaker(badgeData),
+      {
+        headers: {
+          'Content-Type': 'image/svg+xml',
+          'Cache-Control': `max-age=${60 * 5}`,
+          'Content-Disposition': 'inline',
+        },
+      }
+    ));
 }
